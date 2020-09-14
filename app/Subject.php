75984298700dbb3d5160eafff26d06e29bc0bc76
KhinHninSuMon <khinhninsumon@ucssittway.edu.mm>
2020-09-15 01:01:04 +0630

    okok

--- app/Subject.php
+++ app/Subject.php
@@ -7,12 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     protected $fillable = [
-    'name', 'course_id'
+    'name','course_id',
   ];
-
   public function courses($value='')
   {
   	return $this->belongsTo('App\Course');
   }
-
 }
