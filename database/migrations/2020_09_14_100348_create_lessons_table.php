1392cc9fcec85af03a05dbff507c7664fcde09b4
khinekhinehtay <khaingkhainghtay@ucssittway.edu.mm>
2020-09-14 17:10:04 +0630

    create Course model and Lesson model

---
+++ database/migrations/2020_09_14_100348_create_lessons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->text('video');
+            $table->text('description');
+            $table->unsignedBigInteger('subject_id');
+            $table->timestamps();
+            $table->foreign('subject_id')
+                    ->references('id')
+                    ->on('subjects')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
